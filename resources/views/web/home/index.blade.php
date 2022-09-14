9cb078580d0c18cf3cff78144be8a115b259cb7f
BOT <hello@numaan.xyz>
2022-09-14 17:04:35 +0600

    Removed breaking news heading
    Removed breaking news heading from home page as per client request

--- resources/views/web/home/index.blade.php
+++ resources/views/web/home/index.blade.php
@@ -7,11 +7,6 @@
         <!-- Content -->
         <div class="basis-2/3 p-4">
 
-            <div class="mb-5 bg-red-600 p-2 text-white animate-pulse">
-                <b>Breaking News: </b>
-                 Something new for PHP is coming up...
-            </div>
-
             <!-- Post Card -->
             @foreach ($posts as $post)
             <x-web.post-card :post="$post" />
